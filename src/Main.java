4c7ed914609e7348da2cba1b5239c7115e2ad439
Amal <amalabdurahiman@gmail.com>
2023-04-24 14:07:38 +0100

    Attend and Delete Update

--- src/Main.java
+++ src/Main.java
@@ -10,35 +10,17 @@ public class Main {
             System.out.println("Menu");
             System.out.println("1. Reserve Fitness Lesson\n2. Change/Delete a Reservation\n3. Attend a Lesson\n4. Monthly Report\n5. Monthly Champion Fitness Report\n6. Exit\n" );
             choice= sc.nextInt();
-            switch(choice){
-
-                case 1:
-                    Reservation.ReservationView();
-                    break;
-                case 2:
-                    Delete.DeleteView();
-                    break;
-
-                case 3:
-                    Attend.AttendView();//review
-                    break;
-
-                case 4:
-                    Review.MonthlyReviewView();
-                    break;
-
-                case 5:
-                    Review.ChampionReviewView();
-                    break;
-
-                case 6:
+            switch (choice) {
+                case 1 -> Reservation.ReservationView();
+                case 2 -> Delete.DeleteView();
+                case 3 -> Attend.AttendView();//review
+                case 4 -> Review.MonthlyReviewView();
+                case 5 -> Review.ChampionReviewView();
+                case 6 -> {
                     System.out.println("\nTHANK YOU\n\n");
                     System.exit(0);
-                    break;
-                default:
-                    System.out.println("Wrong Choice\n");
-                    break;
-
+                }
+                default -> System.out.println("Wrong Choice\n");
             }
 
 
--- src/Schedule.java
+++ src/Schedule.java
@@ -6,9 +6,8 @@ public class Schedule {
     static Scanner sc=new Scanner(System.in);
     static int ScheduleDay(int dayChoice){
 
-        switch (dayChoice){
-
-            case 1:
+        switch (dayChoice) {
+            case 1 -> {
                 System.out.println("\nOption\t\tWeek\t\tDay\t\t\t\tClass\t\tTime\t\tPrice(GBP)");
                 System.out.println("---------------------------------------------------------------------------");
                 System.out.println("1\t\t\tWeek 1\t\tSaturday\t\tSpin\t\t10:00\t\t2");
@@ -28,45 +27,41 @@ public class Schedule {
                 System.out.println("15\t\t\tWeek 8\t\tSaturday\t\tSpin\t\t10:00\t\t2");
                 System.out.println("16\t\t\tWeek 8\t\tSaturday\t\tBodysculpt\t\t13:00\t\t3");
                 System.out.println();
-
-                Selected=sc.nextInt();
-
-                if(Selected==1)
+                Selected = sc.nextInt();
+                if (Selected == 1)
                     return 101;
-                else if(Selected==2)
+                else if (Selected == 2)
                     return 102;
-                else if(Selected==3)
+                else if (Selected == 3)
                     return 201;
-                else if(Selected==4)
+                else if (Selected == 4)
                     return 202;
-                else if(Selected==5)
+                else if (Selected == 5)
                     return 301;
-                else if(Selected==6)
+                else if (Selected == 6)
                     return 302;
-                else if(Selected==7)
+                else if (Selected == 7)
                     return 401;
-                else if(Selected==8)
+                else if (Selected == 8)
                     return 402;
-                else if(Selected==9)
+                else if (Selected == 9)
                     return 501;
-                else if(Selected==10)
+                else if (Selected == 10)
                     return 502;
-                else if(Selected==11)
+                else if (Selected == 11)
                     return 601;
-                else if(Selected==12)
+                else if (Selected == 12)
                     return 602;
-                else if(Selected==13)
+                else if (Selected == 13)
                     return 701;
-                else if(Selected==14)
+                else if (Selected == 14)
                     return 702;
-                else if(Selected==15)
+                else if (Selected == 15)
                     return 801;
-                else if(Selected==16)
+                else if (Selected == 16)
                     return 802;
-
-                break;
-
-            case 2:
+            }
+            case 2 -> {
                 System.out.println("\nOption\t\tWeek\t\tDay\t\t\t\tClass\t\tTime\t\tPrice(GBP)");
                 System.out.println("---------------------------------------------------------------------------");
                 System.out.println("1\t\t\tWeek 1\t\tSunday\t\t\tZumba\t\t\t10:00\t\t4");
@@ -86,47 +81,41 @@ public class Schedule {
                 System.out.println("15\t\t\tWeek 8\t\tSunday\t\t\tZumba\t\t\t10:00\t\t4");
                 System.out.println("16\t\t\tWeek 8\t\tSunday\t\t\tYoga\t\t13:00\t\t5");
                 System.out.println();
-
-                Selected=sc.nextInt();
-
-                if(Selected==1)
+                Selected = sc.nextInt();
+                if (Selected == 1)
                     return 103;
-                else if(Selected==2)
+                else if (Selected == 2)
                     return 104;
-                else if(Selected==3)
+                else if (Selected == 3)
                     return 203;
-                else if(Selected==4)
+                else if (Selected == 4)
                     return 204;
-                else if(Selected==5)
+                else if (Selected == 5)
                     return 303;
-                else if(Selected==6)
+                else if (Selected == 6)
                     return 304;
-                else if(Selected==7)
+                else if (Selected == 7)
                     return 403;
-                else if(Selected==8)
+                else if (Selected == 8)
                     return 404;
-                else if(Selected==9)
+                else if (Selected == 9)
                     return 503;
-                else if(Selected==10)
+                else if (Selected == 10)
                     return 504;
-                else if(Selected==11)
+                else if (Selected == 11)
                     return 603;
-                else if(Selected==12)
+                else if (Selected == 12)
                     return 604;
-                else if(Selected==13)
+                else if (Selected == 13)
                     return 703;
-                else if(Selected==14)
+                else if (Selected == 14)
                     return 704;
-                else if(Selected==15)
+                else if (Selected == 15)
                     return 803;
-                else if(Selected==16)
+                else if (Selected == 16)
                     return 804;
-
-                break;
-
-            default:
-                System.out.println("Wrong Choice");
-                break;
+            }
+            default -> System.out.println("Wrong Choice");
         }
 
 
@@ -136,9 +125,8 @@ public class Schedule {
 
     static int TimetableClass(int fitChoice) {
 
-        switch (fitChoice){
-
-            case 1:
+        switch (fitChoice) {
+            case 1 -> {
                 System.out.println("\nOption\t\tWeek\t\tDay\t\t\t\tClass\t\tTime\t\tPrice(GBP)");
                 System.out.println("---------------------------------------------------------------------------");
                 System.out.println("1\t\t\tWeek 1\t\tSaturday\t\tSpin\t\t10:00\t\t2");
@@ -150,29 +138,25 @@ public class Schedule {
                 System.out.println("7\t\t\tWeek 7\t\tSaturday\t\tSpin\t\t10:00\t\t2");
                 System.out.println("8\t\t\tWeek 8\t\tSaturday\t\tSpin\t\t10:00\t\t2");
                 System.out.println();
-
-                Selected=sc.nextInt();
-
-                if(Selected==1)
+                Selected = sc.nextInt();
+                if (Selected == 1)
                     return 101;
-                else if(Selected==2)
+                else if (Selected == 2)
                     return 201;
-                else if(Selected==3)
+                else if (Selected == 3)
                     return 301;
-                else if(Selected==4)
+                else if (Selected == 4)
                     return 401;
-                else if(Selected==5)
+                else if (Selected == 5)
                     return 501;
-                else if(Selected==6)
+                else if (Selected == 6)
                     return 601;
-                else if(Selected==7)
+                else if (Selected == 7)
                     return 701;
-                else if(Selected==8)
+                else if (Selected == 8)
                     return 801;
-
-                break;
-
-            case 2:
+            }
+            case 2 -> {
                 System.out.println("\nOption\t\tWeek\t\tDay\t\t\t\tClass\t\tTime\t\tPrice(GBP)");
                 System.out.println("---------------------------------------------------------------------------");
                 System.out.println("1\t\t\tWeek 1\t\tSaturday\t\tBodysculpt\t\t13:00\t\t3");
@@ -184,29 +168,25 @@ public class Schedule {
                 System.out.println("7\t\t\tWeek 7\t\tSaturday\t\tBodysculpt\t\t13:00\t\t3");
                 System.out.println("8\t\t\tWeek 8\t\tSaturday\t\tBodysculpt\t\t13:00\t\t3");
                 System.out.println();
-
-                Selected=sc.nextInt();
-
-                if(Selected==1)
+                Selected = sc.nextInt();
+                if (Selected == 1)
                     return 102;
-                else if(Selected==2)
+                else if (Selected == 2)
                     return 202;
-                else if(Selected==3)
+                else if (Selected == 3)
                     return 302;
-                else if(Selected==4)
+                else if (Selected == 4)
                     return 402;
-                else if(Selected==5)
+                else if (Selected == 5)
                     return 502;
-                else if(Selected==6)
+                else if (Selected == 6)
                     return 602;
-                else if(Selected==7)
+                else if (Selected == 7)
                     return 702;
-                else if(Selected==8)
+                else if (Selected == 8)
                     return 802;
-
-                break;
-
-            case 3:
+            }
+            case 3 -> {
                 System.out.println("\nOption\t\tWeek\t\tDay\t\t\t\tClass\t\tTime\t\tPrice(GBP)");
                 System.out.println("---------------------------------------------------------------------------");
                 System.out.println("1\t\t\tWeek 1\t\tSunday\t\t\tZumba\t\t10:00\t\t4");
@@ -218,29 +198,25 @@ public class Schedule {
                 System.out.println("7\t\t\tWeek 7\t\tSunday\t\t\tZumba\t\t10:00\t\t4");
                 System.out.println("8\t\t\tWeek 8\t\tSunday\t\t\tZumba\t\t10:00\t\t4");
                 System.out.println();
-
-                Selected=sc.nextInt();
-
-                if(Selected==1)
+                Selected = sc.nextInt();
+                if (Selected == 1)
                     return 103;
-                else if(Selected==2)
+                else if (Selected == 2)
                     return 203;
-                else if(Selected==3)
+                else if (Selected == 3)
                     return 303;
-                else if(Selected==4)
+                else if (Selected == 4)
                     return 403;
-                else if(Selected==5)
+                else if (Selected == 5)
                     return 503;
-                else if(Selected==6)
+                else if (Selected == 6)
                     return 603;
-                else if(Selected==7)
+                else if (Selected == 7)
                     return 703;
-                else if(Selected==8)
+                else if (Selected == 8)
                     return 803;
-
-                break;
-
-            case 4:
+            }
+            case 4 -> {
                 System.out.println("\nOption\t\tWeek\t\tDay\t\t\t\tClass\t\tTime\t\tPrice(GBP)");
                 System.out.println("1\t\t\tWeek 1\t\tSunday\t\t\tYoga\t\t13:00\t\t5");
                 System.out.println("2\t\t\tWeek 2\t\tSunday\t\t\tYoga\t\t13:00\t\t5");
@@ -251,32 +227,25 @@ public class Schedule {
                 System.out.println("7\t\t\tWeek 7\t\tSunday\t\t\tYoga\t\t13:00\t\t5");
                 System.out.println("8\t\t\tWeek 8\t\tSunday\t\t\tYoga\t\t13:00\t\t5");
                 System.out.println();
-
-                Selected=sc.nextInt();
-
-                if(Selected==1)
+                Selected = sc.nextInt();
+                if (Selected == 1)
                     return 104;
-                else if(Selected==2)
+                else if (Selected == 2)
                     return 204;
-                else if(Selected==3)
+                else if (Selected == 3)
                     return 304;
-                else if(Selected==4)
+                else if (Selected == 4)
                     return 404;
-                else if(Selected==5)
+                else if (Selected == 5)
                     return 504;
-                else if(Selected==6)
+                else if (Selected == 6)
                     return 604;
-                else if(Selected==7)
+                else if (Selected == 7)
                     return 704;
-                else if(Selected==8)
+                else if (Selected == 8)
                     return 804;
-
-                break;
-
-
-            default:
-                System.out.println("Wrong Choice");
-                break;
+            }
+            default -> System.out.println("Wrong Choice");
         }
 
 
