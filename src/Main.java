f5643e6011dcc4c1855b9868a43498f12b58b043
Amal <amalabdurahiman@gmail.com>
2023-04-25 14:26:34 +0100

    Testing

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc=new Scanner(System.in);
-        int choice;
         do{
             System.out.println("\n\nWelcome to Fitness Weekend Club (FWC)");
             System.out.println("_________________________________\n");
